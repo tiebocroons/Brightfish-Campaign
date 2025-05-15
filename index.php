f33da63432b46eec429102afbe5d285aa088c3ae
HyphaSlayer <tiebo.croons@gmail.com>
2025-05-15 12:26:22 +0200

    Add initial HTML structure and styles for campaign planner and summary pages

--- index.php
+++ index.php
@@ -3,12 +3,39 @@
 ?>
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>HomePage</title>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&display=swap" />
+<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Days:wght@400&display=swap" />
+        <link rel="stylesheet" href="./CSS/index.css" />
+    </head>
+    <body>
+        <div class="main-container">
+            <div class="group">
+                <span class="text"
+                    >Welcome to the campaign planner tool of Brightfish</span
+                >
+                <div class="wrapper">
+                    <span class="text-2"
+                        >With this tool you can easily and quickly create your cinema
+                        campaign. Choose your goals, select your target audience and get
+                        instant insight into the price and impact of your ad. Whether you're
+                        a small business owner or an experienced marketer, our step-by-step
+                        approach and user-friendly interface make cinema advertising
+                        accessible to everyone.<br /></span
+                    ><span class="text-3"
+                        >“I'm gonna make him an offer he can't refuse.” - The Godfather
+                        (1972) <br /></span
+                    ><span class="text-4"
+                        >Want to make your message irresistible? Let this planner help
+                        you!<br /><br />Click the button below and find out how easy it is
+                        to bring your message to the big screen.</span
+                    >
+                </div>
+                <div class="box"><span class="text-5">Let’s get started</span></div>
+            </div>
+        </div>
+    </body>
 </html>
